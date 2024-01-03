b49a5d6788197a45c9cbc13a3c0587bc887a1b1e
jefersontigik <jefersontigik@gmail.com>
2024-01-03 15:24:17 -0300

    Primeiro HTML teste

--- Projeto_3/UI.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-window = tk.Tk()
-window.title('TKInter Py App')
-window.geometry('300x300')
-newLabel = tk.Label(text = 'Hello, TKInter!')
-newLabel.grid(column = 1, row = 3)
-newLabel.pack(anchor = 'center')
-window.mainloop()
